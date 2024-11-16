05d0a00ff80eb813b6b69537d99aa3f1ab3233f1
Flora Le <flora.le@monash.edu>
2024-11-16 23:07:13 +1100

    supplementary results

---
+++ emia-24h-affect-wm-supp.R
@@ -0,0 +1,96 @@
+source("emia-utils.R")
+
+# concurrent
+m_valence_sub <- readRDS(paste0(outputdir, "m_valence_sub", ".RDS"))
+m_energeticarousal_sub <- readRDS(paste0(outputdir, "m_energeticarousal_sub", ".RDS"))
+m_calmness_sub <- readRDS(paste0(outputdir, "m_calmness_sub", ".RDS"))
+m_wm_sub <- readRDS(paste0(outputdir, "m_wm_sub", ".RDS"))
+
+emia_24h_sub_30_list <- lapply(list(
+  m_valence_sub, m_energeticarousal_sub, m_calmness_sub, m_wm_sub
+), function(X) {
+  
+  X <- summary(X, delta = 30)
+  X[, From := ifelse(From == "SleepInterval", "Sleep", From)]
+  X[, To := ifelse(To == "SleepInterval", "Sleep", To)]
+  
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-30, 30), "*", "")]
+})
+
+emia_24h_sub_5_list <- lapply(list(
+  m_valence_sub, m_energeticarousal_sub, m_calmness_sub, m_wm_sub
+), function(X) {
+  
+  X <- summary(X, delta = 5)
+  X[, From := ifelse(From == "SleepInterval", "Sleep", From)]
+  X[, To := ifelse(To == "SleepInterval", "Sleep", To)]
+  
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-5, 5), "*", "")]
+})
+
+emia_24h_sub_60_list <- lapply(list(
+  m_valence_sub, m_energeticarousal_sub, m_calmness_sub, m_wm_sub
+), function(X) {
+  
+  X <- summary(X, delta = 60)
+  X[, From := ifelse(From == "SleepInterval", "Sleep", From)]
+  X[, To := ifelse(To == "SleepInterval", "Sleep", To)]
+  
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-60, 60), "*", "")]
+  
+})
+
+# graphs
+emia_24h_plot_b_supp <- readRDS(paste0(outputdir, "emia_24h_plot_b_supp", ".RDS"))
+emia_24h_plot_w_supp <- readRDS(paste0(outputdir, "emia_24h_plot_w_supp", ".RDS"))
+
+# prospective
+m_valence_lead_sub <- readRDS(paste0(outputdir, "m_valence_lead_sub", ".RDS"))
+m_energeticarousal_lead_sub <- readRDS(paste0(outputdir, "m_energeticarousal_lead_sub", ".RDS"))
+m_calmness_lead_sub <- readRDS(paste0(outputdir, "m_calmness_lead_sub", ".RDS"))
+m_wm_lead_sub <- readRDS(paste0(outputdir, "m_wm_lead_sub", ".RDS"))
+
+emia_24h_lead_sub_30_list <- lapply(list(
+  m_valence_lead_sub, m_energeticarousal_lead_sub, m_calmness_lead_sub, m_wm_lead_sub
+), function(X) {
+  
+  X <- summary(X, delta = 30)
+  X[, From := ifelse(From == "SleepInterval", "Sleep", From)]
+  X[, To := ifelse(To == "SleepInterval", "Sleep", To)]
+  
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-30, 30), "*", "")]
+})
+
+emia_24h_lead_sub_5_list <- lapply(list(
+  m_valence_lead_sub, m_energeticarousal_lead_sub, m_calmness_lead_sub, m_wm_lead_sub
+), function(X) {
+  
+  X <- summary(X, delta = 5)
+  X[, From := ifelse(From == "SleepInterval", "Sleep", From)]
+  X[, To := ifelse(To == "SleepInterval", "Sleep", To)]
+  
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-5, 5), "*", "")]
+})
+
+emia_24h_lead_sub_60_list <- lapply(list(
+  m_valence_lead_sub, m_energeticarousal_lead_sub, m_calmness_lead_sub, m_wm_lead_sub
+), function(X) {
+  
+  X <- summary(X, delta = 60)
+  X[, From := ifelse(From == "SleepInterval", "Sleep", From)]
+  X[, To := ifelse(To == "SleepInterval", "Sleep", To)]
+  
+  X$Sig <- between(0, X$CI_low, X$CI_high)
+  X[, Sig := ifelse(Sig == FALSE & Delta %in% c(-60, 60), "*", "")]
+  
+})
+
+# graphs
+emia_24h_lead_plot_b_supp <- readRDS(paste0(outputdir, "emia_24h_lead_plot_b_supp", ".RDS"))
+emia_24h_lead_plot_w_supp <- readRDS(paste0(outputdir, "emia_24h_lead_plot_w_supp", ".RDS"))
+
